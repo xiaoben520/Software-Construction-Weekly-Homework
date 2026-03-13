35f4c5ca672ba6130e86362136ae1e16a0610bf0
awaqwq233 <820288706@qq.com>
2026-03-13 14:52:27 +0800

    提交第一次作业

---
+++ 第1次作业/代码和运行文件/sghw1.1-3.6/sghw1.1-3.6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace sghw1._1_3._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("你好！我是李颜铭");
+        }
+    }
+}
---
+++ 第1次作业/代码和运行文件/sghw1.2-3.6/sghw1.2-3.6/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace MyFirstCSharpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lowerBound = 0;
+            int upperBound = 0;
+            try
+            {
+                Console.Write("请输入查找素数的下限数据：");
+                lowerBound = int.Parse(Console.ReadLine());
+                Console.Write("请输入查找素数的上限数据：");
+                upperBound = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("输入无效，请输入整数！");
+                Console.ReadLine();
+                return; 
+            }
+            if (lowerBound > upperBound)
+            {
+                int temp = lowerBound;
+                lowerBound = upperBound;
+                upperBound = temp;
+            }
+
+            Console.WriteLine($"\n[{lowerBound}, {upperBound}] 之间的素数有：");
+
+            int primeCount = 0; 
+
+            // 循环遍历上下限之间的所有数字
+            for (int i = lowerBound; i <= upperBound; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.Write(i + "\t");
+                    primeCount++;
+                    if (primeCount % 10 == 0)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+        static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+            for (int i = 3; i * i <= number; i += 2)
+            {
+                if (number % i == 0)
+                {
+                    return false; 
+                }
+            }
+            return true;
+        }
+    }
+}
